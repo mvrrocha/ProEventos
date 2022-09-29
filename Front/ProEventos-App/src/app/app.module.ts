937182986407a05a2e446eca6e3f650f83939a1e
mvrrocha <34744404+mvrrocha@users.noreply.github.com>
2022-09-29 12:20:10 -0300

    Seção 8: Reactive Forms e Layout (Validators)

--- Front/ProEventos-App/src/app/app.module.ts
+++ Front/ProEventos-App/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { EventoService } from './services/evento.service';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -63,7 +63,7 @@ import { RegistrationComponent } from './components/user/registration/registrati
       progressBar: true
     }),
     FormsModule,
-
+    ReactiveFormsModule
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [EventoService],
--- Front/ProEventos-App/src/app/components/user/perfil/perfil.component.ts
+++ Front/ProEventos-App/src/app/components/user/perfil/perfil.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControlOptions, FormGroup, Validators, FormBuilder, FormControl } from '@angular/forms';
+import { ValidatorField } from '@app/helpers/ValidatorField';
 
 @Component({
   selector: 'app-perfil',
@@ -7,9 +9,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilComponent implements OnInit {
 
-  constructor() { }
+  form!: FormGroup;
 
-  ngOnInit() {
+  get f(): any {
+    return this.form.controls;
   }
 
+  constructor(public fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.validation();
+  }
+
+  public validation(): void {
+
+    const formOptions: AbstractControlOptions = {
+      validators: ValidatorField.MustMatch('senha', 'confirmeSenha')
+    };
+
+    this.form = this.fb.group({
+      titulo: ['', Validators.required],
+      primeiroNome: ['', [Validators.required, Validators.minLength(3)]],
+      ultimoNome: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      telefone: ['', Validators.required],
+      funcao: ['', Validators.required],
+      descricao: ['', [Validators.required, Validators.minLength(20)]],
+      senha: ['', Validators.minLength(6)],
+      confirmeSenha: ['', null]
+    }, formOptions);
+  }
+
+  onSubmit(): void{
+    if(this.form.invalid) {
+      return;
+    }
+  }
+
+  public resetForm(event: any): void {
+    event?.preventDefault();
+    this.form.reset();
+  }
 }
